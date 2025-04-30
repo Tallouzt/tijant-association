978f9f841be9b3dc5642c9af58c62b958955ae10
Tallouzt <tallouztmohamed@gmail.com>
2025-04-30 03:45:59 +0100

    Update script.js
    tijant association

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
- document.addeventlistener("domcontent loaded" ,() =>{
+document.addeventlistener("domcontent loaded" ,() =>{
  	 console.log("site Tijant Association chargé avec succés!");});
+
+
+
+
+
+	const loader=
+	document.getElementBy id('loader');
+	windows.addeventlistener('load,()=>                                                                                        
+	{
+	
+	loader.classlist.add('hidden');
+	});
+
+	//menu burger responsive
+
+
+	const burger=
+	document.queryselector('.burger');
+
+	const nav=
+	document.queryselector('.nav-links');
+	burger.addeventlistener('click',()=>{
+		nav.classlist.toggle('active');
+		});
+
